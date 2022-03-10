ee7977093c23ebc35f4279c2c290921503341e20
annguyen <annguyen.it.utc@gmail.com>
2022-03-10 19:20:45 +0700

    Champion page

--- LeagueOfLegends/Controllers/ChampionsController.cs
+++ LeagueOfLegends/Controllers/ChampionsController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LeagueOfLegends.Models;
 
 namespace LeagueOfLegends.Controllers
 {
     public class ChampionsController : Controller
     {
+        LeagueOfLegendsEntities db = new LeagueOfLegendsEntities();
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public PartialViewResult ChampionsList()
+        {
+            return PartialView(db.Champions.ToList());
         }
     }
 }
